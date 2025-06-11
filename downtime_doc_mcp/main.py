9883247592595cd6b6ace3057d5c16c91d594305
Alvaro Duran <alvaro.duran@kiwi.com>
2025-06-11 15:47:50 +0200

    Fix downtime doc MCP

--- downtime_doc_mcp/main.py
+++ downtime_doc_mcp/main.py
@@ -2,27 +2,26 @@ import httpx
 from mcp.server.fastmcp import FastMCP
 import os
 
+
 mcp = FastMCP("Downtime Document")
 
-GOOGLE_DOC_API_KEY = os.getenv("GOOGLE_DOC_API_KEY")
 DOCUMENT_ID = os.getenv("DOCUMENT_ID")
 
-async def make_google_doc_request(document_id: str, api_key: str) -> dict[str, str]:
+async def make_google_doc_request(document_id: str) -> dict[str, str]:
 
-    url = f"https://docs.googleapis.com/v1/documents/{document_id}"
-    params = {'key': api_key}
+    url = f"https://docs.google.com/document/d/{document_id}/export?format=txt"
 
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(follow_redirects=True) as client:
         try:
-            response = await client.get(url, params=params, timeout=30)
+            response = await client.get(url, timeout=30)
             response.raise_for_status()
-            return response.json()
+            return response.text
         except httpx.HTTPStatusError as error:
             raise Exception(f" HTTP Error: {error}: {response.text}")
 
 @mcp.tool()
 async def downtime_document_content(question: str):
-    content = await make_google_doc_request(document_id=DOCUMENT_ID, api_key=GOOGLE_DOC_API_KEY)
+    content = await make_google_doc_request(document_id=DOCUMENT_ID)
 
     result = f"Question: {question}/n"
     result += f"Content: {content}"
